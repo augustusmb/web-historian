f9580fd45b635ec4cf102af40d150b075060d8fb
augustusmb <augustusmb@gmail.com>
2017-03-01 20:42:12 -0800

    return contents of index.html and hardcode test for archive

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,8 +14,38 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+
+
 };
 
 
 
 // As you progress, keep thinking about what helper functions you can put here!
+
+
+// exports.sendResponse = function(response, data, statusCode) {
+//   statusCode = statusCode || 200;
+//   response.writeHead(statusCode, headers);
+//   response.end(JSON.stringify(data));
+// };
+
+// exports.collectData = function(request, callback) {
+//   var data = '';
+//   request.on('data', function(chunk) {
+//     data += chunk;
+//   });
+//   request.on('end', function() {
+//     callback(JSON.parse(data));
+//   });
+// };
+
+// exports.makeActionHandler = function(actionMap) {
+//   return function(request, response) {
+//     var action = actionMap[request.method];
+//     if (action) {
+//       action(request, response);
+//     } else {
+//       exports.sendResponse(response, '', 404);
+//     }
+//   };
+// };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,52 @@
 var path = require('path');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // var index;
+
+  // fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, data) {
+  //   if (err) {
+  //     console.log('Here is your error: ', err);
+  //   } else {
+  //     // console.log('Here is your data: ', data);
+  //     res.write(data);    
+  //   }
+  // });
+  // // write the head shit with 200 status code response
+  // res.writeHead(200, httpHelper.headers);
+  // // end response html
+
+  // console.log('this is index: ', data);
+  // res.end();
+
+  var statusCode = 200; 
+  var body = [];
+  if (req.url === '/') {
+    var getHtml = fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, data) {
+      if (err) {
+        console.log('Here is your error: ', err);
+      } else {
+        console.log('Here is your data: ', data);
+        res.writeHead(statusCode, httpHelper.headers);
+        res.end(data);
+      }
+    });
+  } else {
+    var getGoogle = fs.readFile('/Users/student/Desktop/hrsf73-web-historian' + '/test/testdata/sites/www.google.com', 'utf8', function(err, data) {
+      if (err) {
+        console.log('Here is your error: ', err);
+      } else {
+        console.log('Here is your data: ', data);
+        res.writeHead(statusCode, httpHelper.headers);
+        res.end(data);
+      }
+    });
+  }
+
+
+  // res.end(archive.paths.list);
 };
+
